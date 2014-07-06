553752684fa7c29b25adf27fab96694f1ebdb5b7
Jon Szymaniak <jon.szymaniak@gmail.com>
2014-07-06 15:28:31 -0400

    libbladeRF: Added additional information to sample format descriptions

--- host/libraries/libbladeRF/include/libbladeRF.h
+++ host/libraries/libbladeRF/include/libbladeRF.h
@@ -1099,32 +1099,50 @@ int CALL_CONV bladerf_xb200_get_path(struct bladerf *dev,
  * Sample format
  */
 typedef enum {
-    BLADERF_FORMAT_SC16_Q11, /**< Signed, Complex 16-bit Q11.
-                               *  This is the native format of the DAC data.
-                               *
-                               *  Samples are interleaved IQ value pairs, where
-                               *  each value in the pair is an int16_t. For each
-                               *  value, the data in the lower bits. The upper
-                               *  bits are reserved.
-                               *
-                               *  When using this format, note that buffers
-                               *  must be at least
-                               *       2 * num_samples * sizeof(int16_t)
-                               *  bytes large
-                               */
-    BLADERF_FORMAT_SC16_Q11_META,   /**< Signed, Complex 16-bit Q11, with metadata parsing support
-                                      *  This is the native format of the DAC data.
-                                      *
-                                      *  Samples are interleaved IQ value pairs, where
-                                      *  each value in the pair is an int16_t. For each
-                                      *  value, the data in the lower bits. The upper
-                                      *  bits are reserved.
-                                      *
-                                      *  When using this format, note that buffers
-                                      *  must be at least
-                                      *       2 * num_samples * sizeof(int16_t)
-                                      *  bytes large
-                                      */
+    /**
+     * Signed, Complex 16-bit Q11. This is the native format of the DAC data.
+     *
+     * Values in the range [-2048, 2048) are used to represent [-1.0, 1.0).
+     * Note that the lower bound here is inclusive, and the upper bound is
+     * exclusive. Ensure that provided samples stay within [-2048, 2047].
+     *
+     * Samples consist of interleaved IQ value pairs, with I being the first
+     * value in the pair. Each value in the pair is a right-aligned,
+     * little-endian int16_t. The FPGA ensures that these values are
+     * sign-extended.
+     *
+     * When using this format the minimum required buffer size, in bytes, is:
+     * <pre>
+     *   buffer_size_min = [ 2 * num_samples * sizeof(int16_t) ]
+     * </pre>
+     *
+     * For example, to hold 2048 samples, a buffer must be at least 8192 bytes
+     * large.
+     */
+    BLADERF_FORMAT_SC16_Q11,
+
+    /**
+     * This format is the same as the BLADERF_FORMAT_SC16_Q11 format, except the
+     * first 4 samples (16 bytes) in every block of 1024 samples are replaced
+     * with metadata, organized as follows, with all fields being little endian
+     * byte order:
+     *
+     * <pre>
+     *  0x00 [uint32_t:  Reserved]
+     *  0x04 [uint64_t:  64-bit Timestamp]
+     *  0x0c [uint32_t:  BLADERF_META_FLAG_* flags]
+     * </pre>
+     *
+     * When using the bladerf_sync_rx() and bladerf_sync_tx() functions,
+     * this detail is transparent to caller. These functions take care of
+     * packing/unpacking the metadata into/from the data, via the
+     * bladerf_metadata structure.
+     *
+     * Currently, when using the asynchronous data transfer interface, the user
+     * is responsible for manually packing/unpacking this metadata into/from
+     * their sample data.
+     */
+    BLADERF_FORMAT_SC16_Q11_META,
 } bladerf_format;
 
 /**
